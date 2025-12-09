362324f79411bdcb851345bbafd1706ec335ed93
Inessa Babenko <iness.tt.eln@gmail.com>
2025-12-09 10:17:12 +0700

    feat: user controller class, get users

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,26 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/films")
+public class UserController {
+    private final Map<Long, User> users = new HashMap<>();
+
+    @GetMapping
+    public Collection<User> getUsers() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        if (user.getEmail().isBlank() || !user.getEmail().contains("@")) throw new ConditionsNotMetException("Incorrect email format");
+    }
+
+}
