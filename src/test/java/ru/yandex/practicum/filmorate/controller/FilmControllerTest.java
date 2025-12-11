15904fd0bca34b5cba1b5eb6fb0dc2b14671ed1b
Inessa Babenko <iness.tt.eln@gmail.com>
2025-12-11 21:03:56 +0700

    feat: add tests

---
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -0,0 +1,107 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.Builder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+
+public class FilmControllerTest {
+    private  FilmController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new FilmController();
+    }
+
+    @Test
+    void shouldCreateFilmAllValidField() {
+        Film film = Film.builder()
+                .name("Film")
+                .description("Film description")
+                .releaseDate(LocalDate.now())
+                .duration(30)
+                .build();
+
+        controller.createFilm(film);
+        Assertions.assertEquals(film.getName(),controller.getFilms().stream().findFirst().get().getName());
+        Assertions.assertEquals(film.getDescription(),controller.getFilms().stream().findFirst().get().getDescription());
+        Assertions.assertEquals(film.getReleaseDate(),controller.getFilms().stream().findFirst().get().getReleaseDate());
+        Assertions.assertEquals(film.getDuration(),controller.getFilms().stream().findFirst().get().getDuration());
+    }
+
+    @Test
+    void shouldNoTCreateFilmNameInvalid() {
+        Film film = Film.builder()
+                .name("")
+                .description("Film description")
+                .releaseDate(LocalDate.now())
+                .duration(30)
+                .build();
+
+        controller.createFilm(film);
+        Assertions.assertNotEquals(0,controller.getFilms().size());
+    }
+
+    @Test
+    void shouldNoTCreateFilmReleaseDateInvalid() {
+        Film film = Film.builder()
+                .name("Film")
+                .description("Film description")
+                .releaseDate(LocalDate.of(1890, 11,12))
+                .duration(30)
+                .build();
+
+        Assertions.assertThrows(ValidationException.class, () -> controller.createFilm(film),
+                "Release date should be set after 28 Dec 1895");
+    }
+
+    @Test
+    void shouldUpdateFilm() {
+        Film film = Film.builder()
+                .name("Film")
+                .description("Film description")
+                .releaseDate(LocalDate.of(1990, 11,12))
+                .duration(30)
+                .build();
+
+        Film film2 = Film.builder()
+                .id(1)
+                .name("Film 2 ")
+                .description("Film description 2 ")
+                .releaseDate(LocalDate.of(1995, 5,6))
+                .duration(90)
+                .build();
+
+        controller.createFilm(film);
+        controller.updateFilm(film2);
+
+        Assertions.assertEquals(film2, controller.getFilms().stream().findFirst().get());
+    }
+
+    @Test
+    void shouldReturnAllFilms() {
+        Film film = Film.builder()
+                .name("Film")
+                .description("Film description")
+                .releaseDate(LocalDate.of(1990, 11,12))
+                .duration(30)
+                .build();
+
+        Film film2 = Film.builder()
+                .id(1)
+                .name("Film 2 ")
+                .description("Film description 2 ")
+                .releaseDate(LocalDate.of(1995, 5,6))
+                .duration(90)
+                .build();
+
+        controller.createFilm(film);
+        controller.createFilm(film2);
+
+        Assertions.assertEquals(2, controller.getFilms().size());
+    }
+}
