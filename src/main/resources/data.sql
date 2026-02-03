78688c512ea1e0fa1a65fda52a15726aa7092603
Inessa Babenko <iness.tt.eln@gmail.com>
2026-02-03 08:18:39 +0700

    feat: add data base

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO ratings (mpa_name) VALUES (G) (PG) (PG-13) (R) (NC-17);
+INSERT INTO ratings (mpa_name) VALUES ('G'), ('PG'), ('PG-13'), ('R'), ('NC-17');
 
-INSERT INTO genres (name) VALUES
+INSERT INTO genres (name) VALUES ('Комедия'), ('Драма'), ('Мультфильм'), ('Триллер'), ('Документальный'), ('Боевик');
