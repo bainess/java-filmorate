9421c1f5aac9cba6d1bc6b9a4bb4e21a7b2920d7
Inessa Babenko <iness.tt.eln@gmail.com>
2026-02-05 07:33:28 +0700

    feat: dto user models

---
+++ src/main/java/ru/yandex/practicum/filmorate/dto/user/UpdateUserRequest.java
@@ -0,0 +1,28 @@
+package ru.yandex.practicum.filmorate.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+public class UpdateUserRequest {
+    private Long id;
+
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Should not contain spaces")
+    private String login;
+
+    private String name;
+
+    @Past(message = "Date of birth should be in the past")
+    private LocalDate birthday;
+
+    private Set<Long> friends = new HashSet<>();
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/dto/user/UserDto.java
@@ -0,0 +1,30 @@
+package ru.yandex.practicum.filmorate.dto.user;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+public class UserDto {
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long id;
+
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Should not contain spaces")
+    private String login;
+
+    private String name;
+
+    @Past(message = "Date of birth should be in the past")
+    private LocalDate birthday;
+
+    private Set<Long> friends = new HashSet<>();
+}
