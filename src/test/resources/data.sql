f8b52c1614e2dadfadd7348033fbb54d9ddc6a33
viktoralex2000 <viktoralex2000@gmail.com>
2026-02-18 21:57:27 +0400

    add film-directors mechanicks 1.2

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -2,5 +2,10 @@ MERGE INTO ratings (mpa_name) KEY(mpa_name) VALUES ('G'), ('PG'), ('PG-13'), ('R
 
 MERGE INTO genres (name) KEY(name) VALUES ('Комедия'), ('Драма'), ('Мультфильм'), ('Триллер'), ('Документальный'), ('Боевик');
 
+MERGE INTO event_operations(operation_name) KEY(operation_name) VALUES('REMOVE'), ('ADD'), ('UPDATE');
+
+MERGE INTO event_types(event_name) KEY(event_name) VALUES('LIKE'), ('REVIEW'), ('FRIEND');
+
+
 --INSERT INTO films(name, description, release_date, duration) VALUES ('Title', 'Film description', '1990-11-12', 90);
 --INSERT INTO users(name, login, email, birthday) VALUES ('name', 'login', 'ihn@mail.ru', '1990-11-12');
