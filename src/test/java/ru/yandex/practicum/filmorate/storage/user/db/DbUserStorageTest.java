d9b1e2b5feb8ac09c1942b561c5dd87d4a83b377
Inessa Babenko <iness.tt.eln@gmail.com>
2026-02-24 15:49:47 +0700

    fix: tests

--- src/test/java/ru/yandex/practicum/filmorate/storage/user/db/DbUserStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/user/db/DbUserStorageTest.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.storage.user.db;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
